8a2e4e4bc1ed0741461f25dc9bdd68ef0bc2fa92
N Jedidiah <ntaryebwa.mukama@gmail.com>
2024-05-04 13:59:51 +0300

    new

--- backend/uberclone/routes/api.php
+++ backend/uberclone/routes/api.php
@@ -1,13 +1,17 @@
 <?php
 
-// use App\Http\Controllers\DriverController;
-use App\Http\Controllers\Api\Auth\LoginController;
-// use App\Http\Controllers\TripController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\LoginController;
+// use App\Http\Controllers\DriverController;
+// use App\Http\Controllers\TripController;
 
 Route::post('/login', [LoginController::class, 'submit']);
 Route::post('/login/verify', [LoginController::class, 'verify']);
+
+Route::get('/apple',function(){
+    return "Test api";
+});
 
 // Route::group(['middleware' => 'auth:sanctum'], function () {
 //     Route::get('/driver', [DriverController::class, 'show']);
